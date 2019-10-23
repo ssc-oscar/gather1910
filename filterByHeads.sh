1bda1a1c59cd3c98b0c26540f9022d7c64c6aaf6
Audris Mockus <audris@utk.edu>
2019-10-23 16:08:08 -0400

    addin date for drupal

--- filterByHeads.sh
+++ filterByHeads.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in drupal.com.heads sf$DT.prj.new.heads repo.or.cz.$DT.heads gl$DT.new.heads gitlab.gnome.org.$DT.heads git.zx2c4.com.$DT.heads git.savannah.gnu.org.$DT.heads git.postgresql.org.$DT.heads git.kernel.org.$DT.heads git.eclipse.org.$DT.heads cgit.kde.org.$DT.heads bioconductor.org.$DT.heads android.googlesource.com.$DT.heads git.postgresql.org.$DT.heads; 
+for f in drupal.com.$DT.heads sf$DT.prj.new.heads repo.or.cz.$DT.heads gl$DT.new.heads gitlab.gnome.org.$DT.heads git.zx2c4.com.$DT.heads git.savannah.gnu.org.$DT.heads git.postgresql.org.$DT.heads git.kernel.org.$DT.heads git.eclipse.org.$DT.heads cgit.kde.org.$DT.heads bioconductor.org.$DT.heads android.googlesource.com.$DT.heads git.postgresql.org.$DT.heads;
 do zcat $f | grep -v 'could not connect' | \
 	 perl -ane 'chop(); ($u, @h) = split (/\;/, $_, -1); for $h0 (@h){print "$h0;$#h;$u\n" if $h0=~m|^[0-f]{40}$|}' | \
 	 ssh da4 ~/bin/hasObj1.perl commit | cut -d\; -f3 | uniq > $f.get 
